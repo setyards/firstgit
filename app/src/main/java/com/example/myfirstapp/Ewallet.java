6a0323736dbdb81ccd43371351367a3f83ba72b9
mfarrasmajid <m.farras.majid@gmail.com>
2019-02-20 12:46:31 +0700

    Display Version v1.1

--- app/src/main/java/com/example/myfirstapp/Ewallet.java
+++ app/src/main/java/com/example/myfirstapp/Ewallet.java
@@ -2,6 +2,8 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Ewallet extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Ewallet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ewallet);
+
+        ImageView close = (ImageView) findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                // Your code
+                finish();
+            }
+        });
     }
 }
